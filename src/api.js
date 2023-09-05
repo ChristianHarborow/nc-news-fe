be9ed085a48db9c9cb3c2c7ccd5fcd4f923045cc
ChristianHarborow <christianharborow@hotmail.co.uk>
2023-09-05 15:52:20 +0100

    adds get article comments function

--- src/api.js
+++ src/api.js
@@ -11,3 +11,9 @@ export function getArticle(article_id) {
     .get(`https://nc-news-gdc6.onrender.com/api/articles/${article_id}`)
     .then(({data}) => data.article) 
 }
+
+export function getArticleComments(article_id) {
+    return axios
+    .get(`https://nc-news-gdc6.onrender.com/api/articles/${article_id}/comments`)
+    .then(({data}) => data.comments) 
+}
