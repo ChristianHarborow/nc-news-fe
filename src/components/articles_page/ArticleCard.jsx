624ab96ac329833020337a2cd6b90e419374f919
ChristianHarborow <christianharborow@hotmail.co.uk>
2023-10-04 12:25:03 +0100

    removes commented code

--- src/components/articles_page/ArticleCard.jsx
+++ src/components/articles_page/ArticleCard.jsx
@@ -9,17 +9,6 @@ export default function ArticleCard({article}) {
     const {article_id, topic, title, author, created_at, article_img_url, votes, comment_count} = article
     
     return (
-        // <article className="articleCard">
-        //     <div className="clickable" onClick={() => navigate(`/article/${article_id}`)}>
-        //         <h2>{`${toTitle(topic)} | ${title}`}</h2>
-        //         <p>{`${author} | ${cleanDateTime(created_at)}`}</p>
-        //         <img className="articleImg" src={article_img_url} alt="" />
-        //     </div>
-        //     <div className="voteCommentBar">
-        //         <CommentCounter comments={comment_count}/>
-        //         <VoteCounter startingVotes={votes} parent_id={article_id}/>
-        //     </div>
-        // </article>
         <Card className="articleCard" >
             <CardActionArea>
             <CardContent className="cardContent">
